b38526674402de283873cf0e2b7aeeb3c4c1cea4
TyDaDev <tyrone.cartwright@yahoo.com>
2024-10-10 15:39:05 -0400

    wip: updated homepage colors, added eventListeners & removeListeners and page added page content

--- app.js
+++ app.js
@@ -14,17 +14,53 @@ const newPara = '<p>My advice for new Udacity students is to take your time and 
 subHeading.insertAdjacentHTML('afterend', newPara);
 
 // Update the Home Page Colors
-const startNow = document.getElementById('start-now');
-startNow.style.cssText = 'background-color: #2015ff; border-radius: 5em';
+const startNow = document.querySelector('#start-now');
+startNow.style.backgroundColor = '#2015ff';
+startNow.style.borderRadius = '5em';
 // Select the element with the hero--homepage class
 const heroHome = document.querySelector('.hero--homepage');
 // Use the .add method on the element's classList property to add the new-hero class
 heroHome.classList.add('new-hero');
 // Use appendChild to add the element to the page
-document.body.appendChild(heroHome);
+// document.body.appendChild(heroHome);
 // Create a NodeList of all elements with the .card class
 const cards = document.querySelectorAll('.card');
 // Loop through the NodeList and add the new-card class to each element
 for (let i = 0; i < cards.length; i++) {
   cards[i].classList.add('new-card');
 }
+
+// ADDING INTERACTIVITY
+// document.addEventListener('click', function() {
+//   const mainHeading = document.querySelector('h1');
+  
+//   mainHeading.style.backgroundColor = 'red';
+// })
+
+// document.addEventListener('DOMContentLoaded', function () {
+// });
+const arHeading = document.querySelector('h1');  
+
+const sameText = arHeading.textContent;
+  
+const newText = 'Build an AR App';
+ 
+arHeading.addEventListener('mouseover', function () {
+  arHeading.textContent = newText;    
+});
+arHeading.addEventListener('mouseout', function () {
+  arHeading.textContent = sameText;    
+});  
+
+
+const heroMod = document.querySelector('.hero__module');
+function removeHeroMod() { 
+  heroMod.lastElementChild.remove();
+  document.removeEventListener('click', removeHeroMod);
+}
+
+document.addEventListener('click', removeHeroMod);
+
+// function removeHeroMod() { 
+//   heroMod.lastElementChild.remove();
+// }
